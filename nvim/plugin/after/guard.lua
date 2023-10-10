fbe2ecf57b4cad06d5d4de96733866c9b3c5df17
guilherme096 <guilherme.rosa60@gmail.com>
2023-10-10 23:13:43 +0100

    W

--- nvim/plugin/after/guard.lua
+++ nvim/plugin/after/guard.lua
@@ -4,6 +4,7 @@ ft("python"):fmt("black"):lint("pylint")
 ft("lua"):fmt("stylua"):lint("luacheck")
 ft("c"):fmt("clang-format")
 ft("html,typescript,css,markdown,javascript"):fmt("prettier")
+ft("go"):fmt("gofmt")
 require("guard").setup({
 	fmt_on_save = true,
 	lsp_as_default_formatter = false,
--- nvim/plugin/after/lsp.lua
+++ nvim/plugin/after/lsp.lua
@@ -7,6 +7,7 @@ lsp.ensure_installed({
 	"pyright",
 	"lua_ls",
 	"clangd",
+	"gopls",
 })
 
 -- Fix Undefined global 'vim'
