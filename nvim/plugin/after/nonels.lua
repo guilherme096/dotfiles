3dc868cce00072a9ebceb82b4326a946ee9f7b09
guilherme096 <guilherme.rosa60@gmail.com>
2024-03-30 12:03:54 +0000

    changed to gruvbox again

--- nvim/plugin/after/nonels.lua
+++ nvim/plugin/after/nonels.lua
@@ -9,11 +9,26 @@ local sources = {
         filetypes = { "go" },
     }),
     null_ls.builtins.formatting.prettier.with({
-        filetypes = { "javascript", "typescript", "typescriptreact", "json", "yaml", "markdown" },
+        filetypes = { "javascript", "typescript", "typescriptreact", "json", "yaml", "markdown", "astro" },
     }),
 
 }
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
     sources = sources,
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                    -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+                    vim.lsp.buf.format({async = false})
+                end,
+            })
+        end
+    end,
 })
