2c83eed00d66790501eb76f32efb1bc93e1470f1
guilherme096 <guilherme.rosa60@gmail.com>
2024-03-30 23:04:16 +0000

    fix mappings in telescope

--- nvim/plugin/after/telescope.lua
+++ nvim/plugin/after/telescope.lua
@@ -1,3 +1,18 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+
+require('telescope').setup {
+    defaults = {
+        layout_config = {
+            prompt_position = "top",
+        },
+        mappings = {
+            i = {
+                ["<C-j>"] = require('telescope.actions').move_selection_next,
+                ["<C-k>"] = require('telescope.actions').move_selection_previous,
+            },
+        },
+    },
+}
